fad5fae1a59b26b626dfa2b6f0dca77f3b05f194
Yar59 <75260156+Yar59@users.noreply.github.com>
2023-03-23 16:30:41 +0300

    add archive handler

--- server.py
+++ server.py
@@ -1,5 +1,42 @@
-from aiohttp import web
+import os
+
 import aiofiles
+import asyncio
+import datetime
+from aiohttp import web
+from asyncio import subprocess
+
+INTERVAL_SECS = 1
+
+
+async def create_archive(dir_path):
+    process = await subprocess.create_subprocess_exec(
+        "zip", "-j", "-r", "-", dir_path,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE
+    )
+    return process
+
+
+async def archive_handler(request):
+    dir_name = request.match_info.get('archive_hash')
+    dir_path = os.path.join('test_photos', dir_name)
+    process = await create_archive(dir_path)
+
+    response = web.StreamResponse()
+
+    response.headers['Content-Type'] = 'multipart/form-data;'
+    response.headers['Content-Disposition'] = f'attachment; filename="{dir_name}.zip"'
+
+    await response.prepare(request)
+
+    while True:
+
+        await response.write(await process.stdout.read(102400))
+
+        if process.stdout.at_eof():
+            break
+    return response
 
 
 async def archive(request):
@@ -16,6 +53,6 @@ if __name__ == '__main__':
     app = web.Application()
     app.add_routes([
         web.get('/', handle_index_page),
-        web.get('/archive/{archive_hash}/', archive),
+        web.get('/archive/{archive_hash}/', archive_handler),
     ])
     web.run_app(app)
